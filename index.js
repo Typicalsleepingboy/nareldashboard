e0aba3e76cee2b121135b74713af55ec7c030585
Typicalsleepingboy.exe <99948517+Typicalsleepingboy@users.noreply.github.com>
2025-04-24 23:05:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ client.on('ready', () => {
   setInterval(() => kirimPromosi(client), 5 * 1000); 
 });
 
-client.login('NzI1ODA1Mjk0NzU5ODM3NzM2.GajJhD.HjebLvweHct7t8TKPGQr3QZjTMM8kos0ry83n4');
+client.login('');
